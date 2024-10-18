4496c8108f35f1b056e9a1c309f347cbdd5fb3cc
Francesco Refolli <f.refolli@campus.unimib.it>
2024-10-18 10:40:21 +0200

    packaging for abs

--- src/packer/packaging.cc
+++ src/packer/packaging.cc
@@ -108,19 +108,88 @@ namespace packer::rpmbuild {
 }
 
 namespace packer::makepkg {
-  std::optional<std::string> download_source(const std::string& /*homedir*/, packer::Package* /*package*/) {
-    throw_warning(MSG("packer::makepkg::download_source is not implemented"));
-    return std::nullopt;
+  std::optional<std::string> download_source(const std::string& homedir, packer::Package* package) {
+    std::string url = MSG(package->locator.url << "/archive/refs/heads/master.tar.gz").str();
+    std::string filepath = std::filesystem::path(MSG(homedir << "/makepkg/SOURCES/" << package->locator.name <<".tar.gz").str());
+    if (!std::filesystem::exists(filepath)) {
+      if (!packer::download_file(url, filepath)) {
+        return std::nullopt;
+      }
+    }
+    return filepath;
   }
   
-  std::optional<std::string> craft_pkgbuild(const std::string& /*homedir*/, packer::Package* /*package*/, const std::string& /*source_file*/) {
-    throw_warning(MSG("packer::makepkg::craft_pkgbuild is not implemented"));
-    return std::nullopt;
+  std::optional<std::string> craft_pkgbuild(const std::string& homedir, packer::Package* package) {
+    std::string filepath = std::filesystem::path(MSG(homedir << "/makepkg/PKGBUILDS/" << package->locator.name << ".pkgbuild").str());
+    auto buffer = MSG("");
+    buffer << "# Contributor & Maintainer: Refolli Francesco <francesco.refolli@gmail.com>" << std::endl;
+    buffer << "pkgname=" << package->locator.name << "" << std::endl;
+    buffer << "pkgver=" << package->version << "" << std::endl;
+    buffer << "pkgrel=1" << std::endl;
+    buffer << "pkgdesc='" << package->summary << "'" << std::endl;
+    buffer << "license=('" << package->license << "')" << std::endl;
+    buffer << "arch=('x86_64')" << std::endl;
+
+    if (!(package->build_packer_requirements.empty() && package->build_system_requirements.empty())) {
+      buffer << "makedepends=(";
+      for (packer::Package* dependency : package->build_packer_requirements) {
+        buffer << " '" << dependency->locator.name << "'";
+      }
+      for (std::string dependency : package->build_system_requirements) {
+        buffer << " '" << dependency << "'";
+      }
+      buffer << ")" << std::endl;
+    }
+
+    if (!(package->install_packer_requirements.empty() && package->install_system_requirements.empty())) {
+      buffer << "depends=(";
+      for (packer::Package* dependency : package->install_packer_requirements) {
+        buffer << " '" << dependency->locator.name << "'";
+      }
+      for (std::string dependency : package->install_system_requirements) {
+        buffer << " '" << dependency << "'";
+      }
+      buffer << ")" << std::endl;
+    }
+
+    buffer << "url=" << package->locator.url << "" << std::endl;
+    buffer << "source=('sources.tar.gz')" << std::endl;
+    buffer << "sha256sums=('SKIP')" << std::endl;
+    buffer << "build() {" << std::endl;
+    buffer << "cd $pkgname-master" << std::endl;
+    buffer << package->build_script << std::endl;
+    buffer << "}" << std::endl;
+    buffer << "package() {" << std::endl;
+    buffer << "cd $pkgname-master" << std::endl;
+    buffer << package->install_script << std::endl;
+    buffer << "}" << std::endl;
+    buffer << "options=(!debug)" << std::endl;
+
+    std::ofstream file (filepath);
+    if (!file.good()) {
+      throw_warning(MSG("unable to open '" << filepath << "' spec-file for writing"));
+      return std::nullopt;
+    }
+    file << buffer.str();
+    file.close();
+    return filepath;
   }
 
-  std::optional<std::string> assemble_package(const std::string& /*homedir*/, const std::string& /*spec_file*/, packer::Package* /*package*/) {
-    throw_warning(MSG("packer::makepkg::assemble_package is not implemented"));
-    return std::nullopt;
+  std::optional<std::string> assemble_package(const std::string& homedir, const std::string& pkgbuild_file, const std::string& source_file, packer::Package* package) {
+    std::string package_file = std::filesystem::path(MSG(package->locator.name << "-" << package->version << "-1-x86_64.pkg.tar.zst").str());
+    if (!std::filesystem::exists(package_file)) {
+      std::string build_dir = std::filesystem::path(MSG(homedir << "/makepkg/BUILDS").str());
+      if (!packer::execute_shell_command(MSG("ln -sf " << pkgbuild_file << " " << build_dir << "/PKGBUILD").str())) {
+        return std::nullopt;
+      }
+      if (!packer::execute_shell_command(MSG("ln -sf " << source_file << " " << build_dir <<  "/sources.tar.gz").str())) {
+        return std::nullopt;
+      }
+      if (!packer::execute_shell_command(MSG("makepkg --sign -D " << build_dir).str())) {
+        return std::nullopt;
+      }
+    }
+    return package_file;
   }
 }
 
@@ -152,17 +221,17 @@ std::optional<std::string> packer::craft_package(const std::string& homedir, pac
       }; break;
     case packer::MAKEPKG:
       {
+        std::optional<std::string> pkgbuild_file = packer::makepkg::craft_pkgbuild(homedir, package);
+        if (!pkgbuild_file.has_value()) {
+          throw_warning(MSG("unable to craft pkgbuild file for '" << package->locator.id() << "' package"));
+          return std::nullopt;
+        }
         std::optional<std::string> source_file = packer::makepkg::download_source(homedir, package);
         if (!source_file.has_value()) {
           throw_warning(MSG("unable to download source for '" << package->locator.id() << "' package"));
           return std::nullopt;
         }
-        std::optional<std::string> pkgbuild_file = packer::makepkg::craft_pkgbuild(homedir, package, source_file.value());
-        if (!pkgbuild_file.has_value()) {
-          throw_warning(MSG("unable to craft pkgbuild file for '" << package->locator.id() << "' package"));
-          return std::nullopt;
-        }
-        std::optional<std::string> abs_package = packer::makepkg::assemble_package(homedir, pkgbuild_file.value(), package);
+        std::optional<std::string> abs_package = packer::makepkg::assemble_package(homedir, pkgbuild_file.value(), source_file.value(), package);
         if (!abs_package.has_value()) {
           throw_warning(MSG("unable to assemble and sign abs package for '" << package->locator.id() << "' package"));
           return std::nullopt;
